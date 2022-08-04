6363310c8b07ba1d6381ceffa7d9bf88db84c669
Guillermo Pages <mail@guillermo.at>
2022-08-04 11:27:25 +0200

    fix: hooks not exported

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import useDependentState from "./hooks/useDependentState";
 import useInterval from "./hooks/useInterval";
 import useMemoDeps from "./hooks/useMemoDeps";
 import useLongPress from "./hooks/useLongPress";
+import { useBrowserLanguage } from "./hooks/useBrowserLanguage";
+import useCountdown from "./hooks/useCountdown";
+import { useLocalizedStrings } from "./hooks/useLocalizedStrings";
 
 export {
+  useBrowserLanguage,
+  useCountdown,
   useDependentState,
   useInterval,
-  useMemoDeps,
+  useLocalizedStrings,
   useLongPress,
+  useMemoDeps,
 };
